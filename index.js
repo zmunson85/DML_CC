71524f52fc97963a9b0e01c212062522b5654d56
Zach Munson <zmunson85@gmail.com>
2022-04-04 07:38:49 -0600

    'update/clean-up'

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const connectDB = require('./config/db')
 const path = require('path');
 const app = express();
 
-
 //connect the Database
 connectDB();
 
@@ -14,16 +13,6 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/superHeros', require('./routes/superHeros'));
-
-
-// //serve static production build to heroku
-// if (process.env.NODE_ENV === 'production') {
-//     //client build folder
-//     //import path, create a path instance navigating to index.html
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-// }
-
 
 const PORT = process.env.PORT || 5000;
 
